35c21aa781f70f6d69ee9af1951894cbbd6ec6f0
katychuang <katychuang@gmail.com>
2019-10-20 21:36:23 -0400

    make for building Java

---
+++ make/java_example/Foo.java
@@ -0,0 +1,8 @@
+public class Foo extends Main {
+    public Foo() {
+        System.out.println("Foo");
+    }
+    public void MyMethod(){
+        System.out.println("Printing from Subclass");
+    }
+}
---
+++ make/java_example/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+    public void MyMethod() {
+        System.out.println("Printing from Super Class");
+    }
+}
